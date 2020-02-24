fac82bc4e0d2607efc5b7082eb6bfb8e9c806d9f
Mac <karina@suenosenred.cl>
2020-02-24 16:39:08 -0300

    desafio1 terminado

--- app/src/main/java/com/desafiolatam/firstapp/MainActivity.java
+++ app/src/main/java/com/desafiolatam/firstapp/MainActivity.java
@@ -1,8 +1,16 @@
 package com.desafiolatam.firstapp;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.squareup.picasso.Picasso;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +18,35 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ImageView miimagen= findViewById(R.id.miImagen);
+
+
+        Picasso.get().load("https:/unsplash.com/photos/nVsvIhl4fSc").into(miimagen);
+
+        Button mibotoncito = findViewById(R.id.mi_button);
+
+
+        mibotoncito.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Usuario Ingresado con éxito", Toast.LENGTH_SHORT).show();
+
+
+                //intentExample();
+
+
+            }
+
+
+        });
     }
-}
+
+    //private void intentExample(){
+        //Intent intent= new Intent(  this, Main2Activity.class);
+        //startActivity(intent);
+    }
+
+
+
+
